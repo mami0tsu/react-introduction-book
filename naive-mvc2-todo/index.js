68d9313bb1548655d951ae1bd7462f205b39e739
mamiotsu <mami0tsu.jp@gmail.com>
2021-11-23 14:51:22 +0900

    Add: View class

--- naive-mvc2-todo/index.js
+++ naive-mvc2-todo/index.js
@@ -49,3 +49,78 @@ class TodoListModel {
     return todo;
   }
 }
+
+class View {
+  /**
+   * Add todo to UI
+   * @param {id: number, task: string, isCheck: boolean} todo
+   */
+  addTodo(todo) {
+    const todoElements = document.getElementById("todo-elements");
+    const todoElement = this._createTodoElement(todo);
+    todoElements.appendChild(todoElement);
+  }
+
+  /**
+   * Check todo
+   * @param {number} id todo's id
+   */
+  check(id) {
+    const todoElement = document.getElementById(`todo-${id}`);
+    todoElement.className = `checked`;
+  }
+
+  /**
+   * Uncheck todo
+   * @param {number} id todo's id
+   */
+  uncheck(id) {
+    const todoElement = document.getElementById(`todo-${id}`);
+    todoElement.className = "";
+  }
+
+  /**
+   * Reset input form
+   */
+  resetTodo() {
+    const input = document.getElementById("task-input");
+    input.value = "";
+  }
+
+  /**
+   * Remove todo
+   * @param {number} id todo's id
+   */
+  removeTodo(id) {
+    const todoElement = document.getElementById(`todo-${id}`);
+    todoElement.remove();
+  }
+
+  /**
+   * Create todo element
+   * @param {id: number, task: string} todo
+   * @return HTML element about todo
+   */
+  _createTodoElement(todo) {
+    const {id, task} = todo;
+
+    const todoElement = document.createElement("li");
+    todoElement.id = `todo-${id}`;
+
+    const checkBoxElement = document.createElement("input");
+    checkBoxElement.type = "checkbox";
+    checkBoxElement.id `checkbox-${id}`;
+    todoElement.appendChild(checkBoxElement);
+
+    const labelElement = document.createElement("label");
+    labelElement.innerText = task;
+    todoElement.appendChild(labelElement);
+
+    const buttonElement = document.createElement("button");
+    buttonElement.innerText = "Delete";
+    buttonElement.id = `button-${id}`;
+    todoElement.appendChild(buttonElement);
+
+    return todoElement;
+  }
+}
